3e13426c1c2135af495186ed3e8199dd5d0286a9
anurag1600 <anuragtiwari1600@gmail.com>
2020-08-20 11:45:47 +0530

    java ki file

---
+++ get.js
@@ -0,0 +1,17 @@
+var element;
+
+//element=document.body;
+
+//element=document.getElementById("anurag").innerText;
+
+//element=document.getElementById("anurag").innerHTML;
+//element=document.getElementById("anurag").getAttribute("style");
+//element=document.getElementById("anurag").getAttributeNode("style");
+//element=document.getElementById("anurag").attributes;
+element=document.getElementById("anurag").attributes[3];
+
+console.log(element);
+
+Let’s be friends 🙂
+
+
